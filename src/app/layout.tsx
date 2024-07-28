4c96776c8959bf0ee168dbc2c88cb35c14e058e8
sh4dow18 <ramses.solano.arias@gmail.com>
2024-07-27 19:11:41 -0600

    add: pages documentation
    Add Main Layout and Home Page Documentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,16 @@
 // Main Layout Stylesheets
-import { BurgerMenu } from "@/components/layout";
 import "./globals.css";
 // Main Layout Requirements
-import type { Metadata } from "next";
 import Link from "next/link";
 import { FooterImage, FooterSection } from "@/components/layout/footer";
-// Main Layout Metadata
-export const metadata: Metadata = {
-  title: "Braitec: Soluciones Tecnológicas",
-  description:
-    "Reparamos computadoras, desarrollamos páginas web personalizadas para freelancers y pequeñas empresas, y ofrecemos reparación de software de celulares. ¡Contáctanos para un servicio rápido y confiable!",
-};
+import { BurgerMenu } from "@/components/layout";
 // Main Layout Function
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Navigation Pages
   const PAGES = (
     <>
       <Link href="/">Inicio</Link>
@@ -28,53 +22,72 @@ export default function RootLayout({
       <Link href="/about-us">Nosotros</Link>
     </>
   );
+  // Main Logo
   const LOGO = <p>BRAITEC</p>;
+  // Return Layout
   return (
     <html lang="en">
       <body>
         <header>
+          {/* Burger Menu that only appears in lowers resolutions */}
           <BurgerMenu pages={PAGES} />
           {LOGO}
+          {/* Main Navigation that only appears in normals resolutions */}
           <nav className="desktop-nav">{PAGES}</nav>
         </header>
         <main>{children}</main>
         <footer>
+          {/* Main Logo in Footer */}
           {LOGO}
+          {/* Location Footer Section */}
           <FooterSection title="Ubicación" isfor="links">
+            {/* Google Maps Icon with Link */}
             <FooterImage
               link="https://maps.app.goo.gl/Uh3aYiwm8Y6PHUsj7"
               icon="google-maps"
             />
+            {/* Uber Icon with Link */}
             <FooterImage
               link="https://m.uber.com/ul/?action=setPickup&dropoff[latitude]=9.9799299&dropoff[longitude]=-84.2412363&dropoff[nickname]=Braitec:%20Soluciones%20Tecnológicas"
               icon="uber"
             />
+            {/* Waze Icon with Link */}
             <FooterImage
               link="https://www.waze.com/en/live-map/directions/cr/alajuela-province/alajuela/braitec-soluciones-tecnologicas?navigate=yes&place=ChIJu4Al3TL5oI8RAtAlY655p3k&utm_campaign=default&utm_medium=lm_share_location&utm_source=waze_website"
               icon="waze"
             />
           </FooterSection>
+          {/* Social Media Footer Section */}
           <FooterSection title="Redes Sociales" isfor="links">
+            {/* Whatsapp Icon with Link */}
             <FooterImage link="https://wa.me/50689414905" icon="whatsapp" />
+            {/* Facebook Icon with Link */}
             <FooterImage
               link="https://www.facebook.com/BraitecCR/"
               icon="facebook"
             />
+            {/* Github Icon with Link */}
             <FooterImage link="https://github.com/sh4dow18" icon="github" />
           </FooterSection>
+          {/* Phone Number Footer Section */}
           <FooterSection title="Teléfono" isfor="links">
+            {/* Phone Number Icon and Link */}
             <FooterImage link="tel:+50689414905" icon="phone" />
           </FooterSection>
+          {/* Credits Footer Section */}
           <FooterSection title="Créditos" isfor="credits">
+            {/* Creator's Signature */}
             <p>
               Página hecha por{" "}
               <Link href="https://digital-me.vercel.app">Ramsés Solano</Link>
             </p>
+            {/* Braitec Copyright */}
             <p>
               Copyright ©{" "}
               <Link href="https://braitec.vercel.app">
                 Braitec: Soluciones Tecnológicas
               </Link>{" "}
+              {/* Last Update */}
               26 de Julio del 2024
             </p>
           </FooterSection>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+// Home Page Requirements
+import { Metadata } from "next";
+// Home Page Metadata
+export const metadata: Metadata = {
+  title: "Braitec: Soluciones Tecnológicas",
+  description:
+    "Reparamos computadoras, desarrollamos páginas web personalizadas para freelancers y pequeñas empresas, y ofrecemos reparación de software de celulares. ¡Contáctanos para un servicio rápido y confiable!",
+};
+// Home Page Main Function
 export default function Home() {
   return <></>;
 }
