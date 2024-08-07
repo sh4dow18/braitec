6823bd3631e1f210d0cc037d23c29cdb9a343f50
sh4dow18 <ramses.solano.arias@gmail.com>
2024-08-07 00:43:29 -0600

    add: services in computer software repair page
    Add Cell Phone Diagnosis and Restore Unlock Cards in Services

--- src/app/cellphone-repair/page.tsx
+++ src/app/cellphone-repair/page.tsx
@@ -1,6 +1,10 @@
 // Cellphone Repair Page Requirements
 import { Metadata } from "next";
-import { TitleSection } from "@/components";
+import {
+  CardsListSection,
+  RepairCardSection,
+  TitleSection,
+} from "@/components";
 // Cellphone Repair Page Metadata
 export const metadata: Metadata = {
   title: "Celulares",
@@ -17,6 +21,29 @@ function CellphoneRepair() {
         title="Celulares"
         subtitle="Aquí se muestran todos los servicios relacionados con la Reparación de Software de Celulares"
       />
+      {/* Services Card List Section */}
+      <CardsListSection title="Servicios">
+        {/* Cell Phone Diagnosis Card Section */}
+        <RepairCardSection
+          title="Diagnostico de Celular"
+          image="cell-phone-diagnosis"
+          price={3000}
+          anotherPrice={{
+            value: 5000,
+            type: "regular",
+          }}
+        />
+        {/* Restore Unlock Card Section */}
+        <RepairCardSection
+          title="Desbloqueo por Restauración"
+          image="restore-unlock"
+          price={18000}
+          anotherPrice={{
+            value: 25000,
+            type: "regular",
+          }}
+        />
+      </CardsListSection>
     </>
   );
 }
