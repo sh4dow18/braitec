4dae79a3890c45c0964479d0ac302cd6b89fbe45
sh4dow18 <ramses.solano.arias@gmail.com>
2024-08-08 00:08:42 -0600

    add: table component to store schedule
    Add Table Component with Small Data and Regular Last Child Props

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,6 +1,9 @@
+// Store Page Stylesheets
+import "@/stylesheets/pages/store.css";
 // Store Page Requirements
 import { Metadata } from "next";
-import { TitleSection } from "@/components";
+import { Table, TitleSection } from "@/components";
+import Image from "next/image";
 // Store Page Metadata
 export const metadata: Metadata = {
   title: "Tienda",
@@ -16,6 +19,51 @@ function Store() {
         title="Tienda"
         subtitle="Aquí se Muestra toda la Información de Nuestra Ubicación"
       />
+      {/* Location Section */}
+      <section>
+        <h2>Nuestra Ubicación</h2>
+        <div>
+          {/* Store Schedule Table */}
+          <Table
+            headersList={[
+              {
+                name: "Día",
+              },
+              {
+                name: "Apertura",
+              },
+              {
+                name: "Cierre",
+              },
+            ]}
+            rowsList={[
+              {
+                dataList: ["Lunes", "07:00am", "09:00pm"],
+              },
+              {
+                dataList: ["Martes", "07:00am", "03:00pm"],
+              },
+              {
+                dataList: ["Miercoles", "07:00am", "03:00pm"],
+              },
+              {
+                dataList: ["Jueves", "07:00am", "03:00pm"],
+              },
+              {
+                dataList: ["Viernes", "07:00am", "03:00pm"],
+              },
+              {
+                dataList: ["Sabado", "07:00am", "09:00pm"],
+              },
+              {
+                dataList: ["Domingo", "07:00am", "09:00pm"],
+              },
+            ]}
+            regularLastChild
+            smallData
+          />
+        </div>
+      </section>
     </>
   );
 }
