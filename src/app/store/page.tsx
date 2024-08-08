221b21f73e03480cc3b151656bbabe05b4bdb655
sh4dow18 <ramses.solano.arias@gmail.com>
2024-08-08 13:07:36 -0600

    add: store image and store location
    Add Image and Location in Store Page

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -23,45 +23,58 @@ function Store() {
       <section>
         <h2>Nuestra Ubicación</h2>
         <div>
-          {/* Store Schedule Table */}
-          <Table
-            headersList={[
-              {
-                name: "Día",
-              },
-              {
-                name: "Apertura",
-              },
-              {
-                name: "Cierre",
-              },
-            ]}
-            rowsList={[
-              {
-                dataList: ["Lunes", "07:00am", "09:00pm"],
-              },
-              {
-                dataList: ["Martes", "07:00am", "03:00pm"],
-              },
-              {
-                dataList: ["Miercoles", "07:00am", "03:00pm"],
-              },
-              {
-                dataList: ["Jueves", "07:00am", "03:00pm"],
-              },
-              {
-                dataList: ["Viernes", "07:00am", "03:00pm"],
-              },
-              {
-                dataList: ["Sabado", "07:00am", "09:00pm"],
-              },
-              {
-                dataList: ["Domingo", "07:00am", "09:00pm"],
-              },
-            ]}
-            regularLastChild
-            smallData
-          />
+          <p>
+            Del Super Cali 50 Metros Norte y 50 Metros Este Penúltima Casa a
+            Mano Derecha Portón Negro, Rincón Herrera, Guácima, Alajuela,
+            Alajuela, Costa Rica, 20105
+          </p>
+          <div>
+            <Image
+              src="/misc/store.jpg"
+              alt="Imagen de Tienda"
+              width={427}
+              height={327}
+            />
+            {/* Store Schedule Table */}
+            <Table
+              headersList={[
+                {
+                  name: "Día",
+                },
+                {
+                  name: "Apertura",
+                },
+                {
+                  name: "Cierre",
+                },
+              ]}
+              rowsList={[
+                {
+                  dataList: ["Lunes", "07:00am", "09:00pm"],
+                },
+                {
+                  dataList: ["Martes", "07:00am", "03:00pm"],
+                },
+                {
+                  dataList: ["Miercoles", "07:00am", "03:00pm"],
+                },
+                {
+                  dataList: ["Jueves", "07:00am", "03:00pm"],
+                },
+                {
+                  dataList: ["Viernes", "07:00am", "03:00pm"],
+                },
+                {
+                  dataList: ["Sabado", "07:00am", "09:00pm"],
+                },
+                {
+                  dataList: ["Domingo", "07:00am", "09:00pm"],
+                },
+              ]}
+              regularLastChild
+              smallData
+            />
+          </div>
         </div>
       </section>
     </>
