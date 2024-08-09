7f61a3f6ae3e2288d299fc00b72ba2dc1cb06244
sh4dow18 <ramses.solano.arias@gmail.com>
2024-08-08 20:08:27 -0600

    add: contact form
    Add Contact Form with Name, E-Mail and Message Inputs

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,5 @@
+// Contact Page Stylesheets
+import "@/stylesheets/pages/contact.css";
 // Contact Page Requirements
 import { Metadata } from "next";
 import { TitleSection } from "@/components";
@@ -17,6 +19,43 @@ function Contact() {
         title="Contacto"
         subtitle="Aqui se Muestran las Opciones Disponibles para Establecer un Contacto con Nosotros"
       />
+      <section>
+        <h2>Formulario de Contacto</h2>
+        <form className="contact-form-container">
+          <section>
+            <label htmlFor="name">Nombre</label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              placeholder="Ramsés Solano"
+            />
+            <small>
+              El Nombre debe tener al Menos 2 Palabras, el Nombre y el Apellido
+            </small>
+          </section>
+          <section>
+            <label htmlFor="email">Correo Electrónico</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="ramses.solano@braitec.cr"
+            />
+            <small>Debe ser un Correo Electrónico Válido</small>
+          </section>
+          <section>
+            <label htmlFor="message">Mensaje</label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Quisiera saber sobre..."
+            />
+            <small>El Mensaje debe Poseer Máximo 255 Caracteres</small>
+          </section>
+          <input type="submit" value="Enviar Mensaje" />
+        </form>
+      </section>
     </>
   );
 }
